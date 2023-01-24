b97b43e982e60981684f8a5ea30eae11409dd7e4
Javier Ang <javierangzt@gmail.com>
2023-01-24 20:45:13 +0800

    added change to make db conenction to store messages

--- main.py
+++ main.py
@@ -1,26 +1,7 @@
-import discord
-from discord.ext import commands
-from settings import *
-from token_settings import *
+from smolbot import *
 
-intents = discord.Intents.all()
-bot = commands.Bot(command_prefix='!', intents=intents)
-print('aa')
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-
-    message_author  = message.author
-    message_details = message.content
-    message_channel = message.channel
-
-    print(f'{message_author} just sent a message in {message_channel}: {message_details}')
-
-    if message.content.startswith('!hi'):
-        print(message.content.startswith('!hello'))
-        await message.channel.send('Hello!')
-
-bot.run(token_id)
+if __name__ == "__main__":
+    smolbot = SmolBot()
+    smolbot.run_bot()
 
 
